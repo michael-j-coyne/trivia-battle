b10d826399bfa577e7658b34f04611f220ac2bd5
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-20 13:17:01 -0500

    Add BrowserRouter, Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>This is a blank project</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>This is the homepage</h1>
+      <p>And this is a paragraph on the homepage</p>
+    </div>
+  );
+}
