0665c4b678333b408804da6caf405145fe5848f1
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-20 17:55:52 -0500

    Render trivia question fetched from API

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -12,10 +12,30 @@
   I'll implement this page first, using some data from a .json file so I can focus on the game flow and ignore the fetching logic for now.
   */
 
+import { useState, useEffect } from "react";
+import Trivia from "../components/Trivia";
+import fetchTrivia from "../fetchTrivia";
+
 export default function Game() {
+  const [trivia, setTrivia] = useState();
+
+  useEffect(() => {
+    fetchTrivia({
+      amount: 1,
+      categories: "society_and_culture,science",
+      difficulties: "easy,medium,hard",
+    })
+      .then((trivia) => setTrivia(trivia))
+      .catch((e) => console.error(e));
+  }, []);
+
+  console.log(trivia);
+
   return (
     <div>
-      <h1>This is the game page</h1>
+      {trivia && (
+        <Trivia question={trivia[0].question} choices={trivia[0].choices} />
+      )}
     </div>
   );
 }
