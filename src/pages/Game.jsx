95b9b33b9ccf37d92a3c24d0709df7b9baeb9969
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-23 14:33:59 -0500

    Delete comment

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,17 +1,3 @@
-/*
-  This page is where users will spend most of their time. The actual "game"
-  takes place here.
-
-  During the game, players divide into two teams, and will take turns
-  answering questions.
-
-  If a team gets a question wrong, the opposing team could get a chance to "steal" a multiple choice question, but they would only get half a point since one of the options has been eliminated already.
-
-  Players could instead choose to "skip" a question, depriving the opposing team of the opportunity to steal.
-
-  I'll implement this page first, using some data from a .json file so I can focus on the game flow and ignore the fetching logic for now.
-  */
-
 import { useState, useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Trivia from "../components/Trivia";
