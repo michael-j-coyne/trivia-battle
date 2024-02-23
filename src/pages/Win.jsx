59dab9104c6c7740283dd3cf5ce817c9677fe20e
michael-coyne-01 <137124477+michael-coyne-01@users.noreply.github.com>
2024-02-23 14:37:17 -0500

    Add "play again" button

--- src/pages/Win.jsx
+++ src/pages/Win.jsx
@@ -1,7 +1,8 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Win() {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const params = new URLSearchParams(location.search);
 
@@ -11,5 +12,23 @@ export default function Win() {
 
   winner === "none" ? (message = "It's a tie!") : (message = `${winner} win!`);
 
-  return <h1 style={{ fontSize: "2.5rem" }}>{message}</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "150px",
+        justifyContent: "space-between",
+      }}
+    >
+      <h1 style={{ fontSize: "2.5rem" }}>{message}</h1>
+      <button
+        style={{ height: "50px", fontSize: "1.25rem" }}
+        onClick={() => navigate("/setup")}
+        className="button button_submit"
+      >
+        Play again
+      </button>
+    </div>
+  );
 }
