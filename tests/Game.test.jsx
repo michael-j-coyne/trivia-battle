0ddc409c8ceb1fc3c884ec775577459c01f9396d
Michael Coyne <137124477+michael-j-coyne@users.noreply.github.com>
2024-03-06 15:25:46 -0500

    Create end-to-end test
    Test the entire game loop.

--- tests/Game.test.jsx
+++ tests/Game.test.jsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter, MemoryRouter, Route, Routes } from "react-router-dom";
 
 import { describe, it, expect, beforeAll } from "vitest";
 import Game from "../src/pages/Game";
+import Win from "../src/pages/Win";
 import Setup from "../src/pages/Setup";
+import Home from "../src/pages/Home";
 
 const teamOneName = "myteamname";
 const teamTwoName = "t2name";
@@ -81,6 +84,10 @@ const mockTrivia = [
   },
 ];
 
+async function fetchMockTrivia() {
+  return mockTrivia;
+}
+
 const mockDataInGame = JSON.stringify({
   turn: 1,
   questionCompleted: false,
@@ -98,7 +105,7 @@ const mockDataInGame = JSON.stringify({
   },
 });
 
-function isCategorySelectScreen() {
+function expectCategorySelectScreen() {
   screen.getByText(/select categories!/i);
   screen.getByRole("button", { name: /music/i });
   screen.getByRole("button", { name: /history/i });
@@ -109,6 +116,29 @@ function isCategorySelectScreen() {
   return true;
 }
 
+function expectHomeScreen() {
+  screen.getByText(/trivia battle!/i);
+  screen.getByRole("button", { name: /get started/i });
+}
+
+function expectTeamOneNameSelectScreen() {
+  screen.getByText(/team one/i);
+  screen.getByText(/pick a name/i);
+
+  screen.getByRole("textbox", { name: /team name/i });
+  screen.getByRole("button", { name: /next/i });
+}
+
+function expectTeamTwoNameSelectScreen() {
+  screen.getByText(/team two/i);
+  screen.getByText(/pick a name/i);
+
+  screen.getByRole("textbox", { name: /team name/i });
+
+  screen.getByRole("button", { name: /go back/i });
+  screen.getByRole("button", { name: /next/i });
+}
+
 function isSetupScreen() {
   screen.getByText(/pick a name/i);
   screen.getByRole("button", { name: /next/i });
@@ -116,7 +146,7 @@ function isSetupScreen() {
   return true;
 }
 
-function isGameScreen() {
+function expectGameScreen() {
   screen.getAllByText(teamOneName, { exact: false });
   screen.getAllByText(teamTwoName, { exact: false });
 
@@ -126,6 +156,30 @@ function isGameScreen() {
   screen.getByRole("button", { name: /option 4/i });
 
   return true;
+}
+
+function expectWinScreen(winner) {
+  screen.getByText(`${winner} win(s)!`, { exact: false });
+  screen.getByRole("button", { name: /play again/i });
+}
+
+function expectStealMode(stealingTeamName) {
+  screen.getByText(`${stealingTeamName}, your chance to steal!`, {
+    exact: false,
+  });
+}
+
+function expectScoreToBe(teamOneScore, teamTwoScore) {
+  screen.getByText(`${teamOneName}: ${teamOneScore}`);
+  screen.getByText(`${teamTwoName}: ${teamTwoScore}`);
+}
+
+function expectQuestionNumberToBe(questionNum) {
+  screen.getByText(`Q ${questionNum} / 10`);
+}
+
+function questionCompleted() {
+  return screen.queryByText(/next question/i) !== null;
 }
 
 describe("Load /game route without query params or saveData", () => {
@@ -161,7 +215,7 @@ describe("Load /game route with query params", () => {
       </MemoryRouter>
     );
 
-    expect(isCategorySelectScreen()).toEqual(true);
+    expect(expectCategorySelectScreen()).toEqual(true);
   });
 });
 
@@ -173,8 +227,127 @@ describe("Load <Game /> without query string params BUT with saveData", () => {
     };
   });
 
-  it("Initializes the Game component using the saveData in localstorage", () => {
+  it("Should initialize the Game component using the saveData in localstorage", () => {
     render(<Game />, { wrapper: BrowserRouter });
-    expect(isGameScreen()).toEqual(true);
+    expect(expectGameScreen()).toEqual(true);
+  });
+});
+
+describe("Play through the game", () => {
+  it("plays through the entire game. Starts at the home page, goes through and picks team names, then starts the game. Tests out steal mode, ensures points are incremented, ensures win screen is displayed at the end.", async () => {
+    const user = userEvent.setup();
+
+    render(
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: "/",
+          },
+        ]}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/setup" element={<Setup />} />
+          <Route
+            path="/game"
+            // FYI: the fetchMockTrivia function does not take category selection into account.
+            element={<Game fetchTrivia={fetchMockTrivia} />}
+          />
+          <Route path="/win" element={<Win />} />
+        </Routes>
+      </MemoryRouter>
+    );
+
+    // utility funcs
+    const goNextQuestion = async () =>
+      await user.click(screen.getByRole("button", { name: /next question/i }));
+    const pickOption = async (optionName) =>
+      await user.click(screen.getByRole("button", { name: optionName }));
+
+    // begin test
+    expectHomeScreen();
+
+    await user.click(screen.getByRole("button", { name: /get started/i }));
+
+    expectTeamOneNameSelectScreen();
+
+    await user.type(
+      screen.getByRole("textbox", { name: /team name/i }),
+      teamOneName
+    );
+    await user.click(screen.getByRole("button", { name: /next/i }));
+
+    expectTeamTwoNameSelectScreen();
+
+    await user.type(
+      screen.getByRole("textbox", { name: /team name/i }),
+      teamTwoName
+    );
+    await user.click(screen.getByRole("button", { name: /next/i }));
+
+    expectCategorySelectScreen();
+    expect(localStorage.getItem("saveData")).toBeNull();
+
+    // we're just going to select a category so that we can start the game, though
+    // selecting the category has no effect on what type of trivia is returned
+    // in this test.
+    await user.click(screen.getByRole("button", { name: /history/i }));
+    await user.click(screen.getByRole("button", { name: /start game/i }));
+
+    expectGameScreen();
+    expectQuestionNumberToBe(1);
+    expect(localStorage.getItem("saveData")).not.toBeNull();
+
+    await pickOption(/option 2/i);
+
+    expectScoreToBe(1, 0);
+    await goNextQuestion();
+    expectQuestionNumberToBe(2);
+
+    await pickOption(/option 3/i);
+    expectStealMode(teamOneName);
+    expectScoreToBe(1, 0);
+
+    await pickOption(/option 2/i);
+    expectScoreToBe(2, 0);
+
+    await goNextQuestion();
+    await pickOption(/option 4/i);
+    expectStealMode(teamTwoName);
+
+    // nothing should happen, button should be disabled
+    await pickOption(/option 4/i);
+    expect(questionCompleted()).toBeFalsy();
+
+    await pickOption(/option 3/i);
+    expect(questionCompleted()).toBeTruthy();
+    expectScoreToBe(2, 0);
+    expectQuestionNumberToBe(3);
+
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    expectScoreToBe(2, 1);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+    await goNextQuestion();
+
+    await pickOption(/option 2/i);
+
+    await user.click(screen.getByRole("button", { name: /next/i }));
+    expectWinScreen(teamOneName);
   });
 });
